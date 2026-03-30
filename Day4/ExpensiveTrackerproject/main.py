1aaaf15a14f00d2ce04ead738a080bf6f29e5318
Sumithreddy <160596444+SSumithreddy@users.noreply.github.com>
2026-03-30 11:57:23 +0530

    strings

---
+++ Day4/ExpensiveTrackerproject/main.py
@@ -0,0 +1,35 @@
+expenses=[]
+def add_expenses():
+    name=input("Enter expense name: ")
+    amount=float(input("Enter amount expense: "))
+    expense={
+        "name": name,
+        "amount": amount
+    }
+    expenses.append(expense)
+    print("Expense added.")
+
+def view_expenses():
+    for expense in expenses:
+        print(expense["name"], "-", expense["amount"])
+    
+def show_menu():
+    print("\nExpense Tracker")
+    print("1. Add Expense")
+    print("2. View Expense")
+    print("3. Exit")
+    
+def main():
+    while True:
+        show_menu()
+        choice = input ("Enter choice: ")
+        if choice == "1":
+            add_expenses()
+        elif choice == "2":
+            view_expenses()
+        elif choice == "3":
+            print("Exiting the program")
+            break
+        else:
+            print("Invalid Choice. Try again!")
+main()
